05b7d4b2abaf329b50465e64045071dd5c980beb
Daniel Reus <daniel.reus@triggi.com>
2021-10-25 03:05:45 +0200

    Initial aftermath page

---
+++ results/DANCE_files/chunk-2d0c42b3.91c1bf93.v1.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c42b3"],{"3a4c":function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("transition",{attrs:{name:"fade",mode:"out-in"}},[t("router-view")],1)},r=[],u=t("2877"),c={},o=Object(u["a"])(c,a,r,!1,null,null,null);e["default"]=o.exports}}]);
